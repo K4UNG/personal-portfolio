47ea791a7920ba2f5dcdc8bab039f7183c62a531
K4UNG <kgzinhein.my@gmail.com>
2022-09-03 21:35:16 +0630

    desktop langs section and portfolio scroll slide in

--- components/home/Portfolio/Portfolio.jsx
+++ components/home/Portfolio/Portfolio.jsx
@@ -1,11 +1,41 @@
 import styles from "./Portfolio.module.css";
 import Projects from "./Projects";
 import Achievements from "./Achievements";
+import { motion, useScroll } from "framer-motion";
+import { useState, useEffect, useRef } from "react";
 
 export default function Portfolio() {
+  const { scrollY } = useScroll();
+  const title = useRef();
+  const [height, setHeight] = useState();
+  const [scroll, setScroll] = useState();
+  const [width, setWidth] = useState();
+
+  scrollY.onChange(() => setScroll(scrollY.current));
+
+  function resizeHandler() {
+    setHeight(title.current.offsetTop);
+    setWidth(window.innerWidth)
+  }
+
+  useEffect(() => {
+    resizeHandler()
+    window.addEventListener('resize', resizeHandler)
+
+    return () => window.removeEventListener('resize', resizeHandler)
+  }, []);
+
   return (
     <section id="portfolio" className={styles.portfolio}>
-      <h2 className={styles.title}>Portfolio</h2>
+      <div ref={title}>
+        <motion.h2
+          style={width >= '1100' ? { x: height - scroll } : {}}
+          transition={{ type: "spring", stiffness: 1 }}
+          className={styles.title}
+        >
+          Portfolio
+        </motion.h2>
+      </div>
       <Projects />
       <Achievements />
     </section>
