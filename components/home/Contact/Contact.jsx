711658aecdf2a57a80330507d33c3ae6cabce1dc
K4UNG <kgzinhein.my@gmail.com>
2022-09-09 13:33:44 +0630

    send email from contact form

--- components/home/Contact/Contact.jsx
+++ components/home/Contact/Contact.jsx
@@ -1,14 +1,16 @@
 import styles from "./Contact.module.css";
-import { useState } from "react";
+import { useRef, useState } from "react";
 import { useDispatch } from "react-redux";
 import { animationActions } from "../../../store/animationSlice";
-import Image from 'next/image';
-import ArrowTopRight from '../../ui/ArrowTopRight';
+import Image from "next/image";
+import ArrowTopRight from "../../ui/ArrowTopRight";
+import emailjs from "@emailjs/browser";
 
 export default function Contact() {
-  const [name, setName] = useState("");
-  const [email, setEmail] = useState("");
-  const [message, setMessage] = useState("");
+  const formRef = useRef();
+  const [error, setError] = useState(null);
+  const [loading, setLoading] = useState(false);
+  const [success, setSuccess] = useState(null);
 
   const dispatch = useDispatch();
 
@@ -23,56 +25,114 @@ export default function Contact() {
     el.classList.remove(styles.focus);
   }
 
+  function validateEmail(email) {
+    return String(email)
+      .toLowerCase()
+      .match(
+        /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/
+      );
+  }
+
   function submitHandler(e) {
     e.preventDefault();
+    setLoading(true);
+    setError(null);
+    setSuccess(null);
+    const {
+      name: { value: name },
+      email: { value: email },
+      message: { value: message },
+    } = formRef.current;
+
+    if (name.trim().length === 0) {
+      setError("Name can't be empty.");
+      setLoading(false);
+      return;
+    } else if (email.trim().length === 0) {
+      setError("Email can't be empty.");
+      setLoading(false);
+      return;
+    } else if (!validateEmail(email)) {
+      setError("Invalid email!");
+      setLoading(false);
+      return;
+    } else if (message.trim().length === 0) {
+      setError("Message can't be empty.");
+      setLoading(false);
+      return;
+    }
+    emailjs
+      .sendForm(
+        process.env.NEXT_PUBLIC_EMAILJS_SERVICE_ID,
+        "template_4ma2kkr",
+        formRef.current,
+        process.env.NEXT_PUBLIC_EMAILJS_PUBLIC_KEY
+      )
+      .then(() => {
+        setSuccess("Successfully sent!");
+        setLoading(false);
+        formRef.current.reset()
+      })
+      .catch(() => {
+        setError("Something went wrong");
+        setLoading(false);
+      });
   }
 
   return (
     <section id="contact" className={styles.contact}>
       <div className={styles.image}>
-        <Image src="/images/letter.jpg" alt='letter box' className={styles.image} height='800' width='700' />
+        <Image
+          src="/images/letter.jpg"
+          alt="letter box"
+          className={styles.image}
+          height="800"
+          width="700"
+        />
         <div className={styles.overlay} />
-        <h2 className={styles.title}>Get in Touch <br/><ArrowTopRight /></h2>
+        <h2 className={styles.title}>
+          Get in Touch <br />
+          <ArrowTopRight />
+        </h2>
       </div>
       <div className={styles.form}>
-        <form onSubmit={submitHandler}>
+        <form noValidate ref={formRef} onSubmit={submitHandler}>
+          {error && <div className={styles.error}>{error}</div>}
+          {success && <div className={styles.success}>{success}</div>}
           <div className={styles.control}>
             <label htmlFor="name">Name</label>
             <input
-              value={name}
               onFocus={focusHandler}
               onBlur={blurHandler}
               id="name"
               type="text"
-              onChange={(e) => setName(e.target.value)}
+              name="name"
             />
           </div>
           <div className={styles.control}>
             <label htmlFor="email">Email</label>
             <input
-              value={email}
               onFocus={focusHandler}
               onBlur={blurHandler}
               id="email"
               type="email"
-              onChange={(e) => setEmail(e.target.value)}
+              name="email"
             />
           </div>
           <div className={styles.control}>
             <label htmlFor="message">Message</label>
             <textarea
-              value={message}
               onFocus={focusHandler}
               onBlur={blurHandler}
               rows="5"
               id="message"
-              onChange={(e) => setMessage(e.target.value)}
+              name="message"
             ></textarea>
           </div>
           <button
             onMouseEnter={() => dispatch(animationActions.hideCursor())}
             onMouseLeave={() => dispatch(animationActions.removeState())}
-            className={styles.button}
+            className={`${styles.button} ${loading && styles.loading}`}
           >
             Send
           </button>
